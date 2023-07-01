f7dd9e6574e16a8a4478147db0183c7a6f60a854
SantiagoBedoya <a.sbedoya200110@gmail.com>
2023-07-01 13:12:53 -0500

    feat: planner with sql db

--- planner/main.py
+++ planner/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
@@ -10,5 +11,11 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 
-if __name__ == '__main__':
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
+
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- planner/routes/events.py
+++ planner/routes/events.py
@@ -1,42 +1,59 @@
-from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
+from models.events import Event, EventUpdate
+from database.connection import get_session
+from sqlmodel import select
 
 event_router = APIRouter(tags=["Events"])
 
-events = []
 
 @event_router.get("/", response_model=List[Event])
-async def get_events()-> List[Event]:
+async def get_events(session=Depends(get_session)) -> List[Event]:
+    stmt = select(Event)
+    events = session.exec(stmt).all()
     return events
 
+
 @event_router.get("/{id}", response_model=Event)
-async def get_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
-    
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event not found"
-    )
+async def get_event(id: int, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        return event
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
+
 
 @event_router.post("/")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
-    return {
-        "message": "event created successfully"
-    }
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+    return {"message": "event created successfully"}
+
+
+@event_router.put("/{id}", response_model=Event)
+async def update_event(
+    id: int, new_data: EventUpdate, session=Depends(get_session)
+) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
+
 
 @event_router.delete("/{id}")
-async def delete_event(id: int)-> dict:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "event deleted successfully"}
+async def delete_event(id: int, session=Depends(get_session)) -> dict:
+    event = session.get(Event, id)
+    if event:
+        session.delete(event)
+        session.commit()
 
-    raise HTTPException(
-        status_code = status.HTTP_404_NOT_FOUND,
-        detail="event not found"
-    )
-
+        return {"message": "Event deleted successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="event not found")
